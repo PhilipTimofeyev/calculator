f9afcd3331540d606e7fc60c4a65fe354b72a214
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-03-27 17:24:17 -0700

    Add eventlistener to buttons and working variable which is sent to display

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,26 @@
 const container = document.querySelector(".container");
 const calculator = document.querySelector(".calculator");
 const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".row-one");
 
+// numbers.forEach(function(elem) {
+// 	console.log(elem);
+//     elem.addEventListener("input", function() {
+//         alert("YAY")
+//     });
+// });
+
+let working = ''
+
+document.addEventListener('click', function(e){
+  if(e.target.tagName=="BUTTON"){
+   working += e.target.innerHTML;
+   display.textContent = working;
+  	// console.log(e.target.innerHTML)
+  }
+})
+
+
+function respond(btn) {	
+	
+}
