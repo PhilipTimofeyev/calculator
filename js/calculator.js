e7a8000e64b4d858b77855b56fbc5d92ab0ceb57
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-03-28 14:33:44 -0700

    Refactor

--- js/calculator.js
+++ js/calculator.js
@@ -2,51 +2,59 @@ const container = document.querySelector(".container");
 const calculator = document.querySelector(".calculator");
 const display = document.querySelector(".display");
 
-display.textContent = '0'
 let workingA = ''
 let workingB = ''
 let lastOperand = ''
 let equalUsed = false
 
+display.textContent = '0'
+
 document.addEventListener('click', function(e){
   if(e.target.tagName=="BUTTON"){respond(e.target.innerHTML)}
-  	// console.log(`A: ${workingA}`)
-  	// console.log(`B: ${workingB}`)
-  	// console.log(`Oper: ${lastOperand}`)
+  	console.log(`A: ${workingA}`)
+  	console.log(`B: ${workingB}`)
+  	console.log(`Oper: ${lastOperand}`)
+  	console.log(`equals: ${equalUsed}`)
 	})
 
 
 function respond(btn) {	
 	const strBtn = String(btn)
 
-	if (btn.match(/[0-9]/g)) {
+	if (checkIfNumber(strBtn)) {
 			workingA += btn;
 			display.textContent = workingA;
+
 	} else if (strBtn == '=') {
-			if (workingB == '') {return}
-			equals(lastOperand);
-			equalUsed = true
-			display.textContent = workingB
+		equals()
+
 	} else if (strBtn == 'AC'){
 			reset()
-	} else {operatorLogic(strBtn); equalUsed = false}
 
+	} else {
+		operatorLogic(strBtn);
+		equalUsed = false;
+	}
 }
 
 function operatorLogic(currentOperand){
 	if (workingB == '') {
-		workingB = Number(workingA)
+		workingB = workingA
+
 	} else if (lastOperand == currentOperand){
-		equals(currentOperand)
+		operate(currentOperand)
+
 	} else if (equalUsed == true) {
 		workingA = ''
-		equals(lastOperand)
-	} else {equals(lastOperand)}
+		operate(lastOperand)
+
+	} else {operate(lastOperand)
+	}
 	workingA = ''
 	lastOperand = currentOperand;
 }
 
-function equals(operand) {
+function operate(operand) {
 	let a = Number(workingA)
 	let b = Number(workingB)
 	let result
@@ -70,30 +78,34 @@ function equals(operand) {
 	display.textContent = workingB;
 }
 
-function add(a, b) {
-	return (a + b)
-}
+function add(a, b) {return (a + b)}
 
-function subtract(a, b) {
-	return (a - b)
-}
+function subtract(a, b) {return (a - b)}
 
-function multiply(a, b) {
-	return (a * b)
-}
+function multiply(a, b) {return (a * b)}
 
 function divide(a, b) {
-	if (b == 0) {
-		return display.textContent = 'NO DIVIDING BY ZERO';
-	}
-	return (a / b)
+	// return (a / b)
+	return b == 0 ? display.textContent = 'NO DIVIDING BY ZERO' : (a / b)
 }
 
-function reset(){
+function equals() {
+	if (workingB == '') {return}
+	operate(lastOperand);
+	equalUsed = true;
+	display.textContent = workingB;
+}
+
+function reset() {
 	workingA = '';
 	workingB = '';
 	lastOperand = '';
 	display.textContent = '0'
+	equalUsed = false
+}
+
+function checkIfNumber(str){
+	return str.match(/[0-9]/g)
 }
 
 
