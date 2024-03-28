b4afb5ec2d9bcdf280369f34caeac346ee6f17b0
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-03-27 20:23:36 -0700

    Add operation methods

--- js/calculator.js
+++ js/calculator.js
@@ -12,6 +12,7 @@ const numbers = document.querySelectorAll(".row-one");
 
 let workingA = ''
 let workingB = ''
+let lastOperand = ''
 
 document.addEventListener('click', function(e){
   if(e.target.tagName=="BUTTON"){
@@ -26,15 +27,46 @@ function respond(btn) {
 		workingA += btn;
 		display.textContent = workingA;
 	} else if (String(btn) == '+') {
+		lastOperand = '+';
 		if (workingB == '') {
 			workingB = Number(workingA)
 			workingA = ''
-			display.textContent = workingA;
 		} else {
 			workingB += Number(workingA)
 			display.textContent = workingB;
 			workingA = ''
 		}
+	} else if (String(btn) == '=') {
+		display.textContent = equals(lastOperand);
+		// equals(lastOperand)
 	}
 }
+
+function equals(operand) {
+	let result
+	switch (operand) {
+	case '+':
+		result = Number(workingA) + Number(workingB)
+	}
+	return result
+}
+
+function add(a, b) {
+	return (a + b)
+}
+
+function subtract(a, b) {
+	return (a - b)
+}
+
+function multiply(a, b) {
+	return (a * b)
+}
+
+function add(a, b) {
+	return (a / b)
+}
+
+
+
 // console.log("Hello1".match(/[0-9]/g))
